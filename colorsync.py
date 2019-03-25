3196d7b1cbb9c78ebd77b45b204a13422190f355
jorgefloresq <jorge.noel.flores@gmail.com>
2019-03-25 10:47:37 -0700

    Refactor code for python best practices

--- colorsync.py
+++ colorsync.py
@@ -17,66 +17,70 @@ username = config.spotifyId
 scope = 'user-read-currently-playing'
 
 try:
-    token = util.prompt_for_user_token(username, scope, client_id= config.CLIENT_ID,
-                                       client_secret= config.CLIENT_SECRET,
-                                       redirect_uri= config.REDIRECT_URI)
+    token = util.prompt_for_user_token(username, scope, client_id=config.CLIENT_ID,
+                                       client_secret=config.CLIENT_SECRET,
+                                       redirect_uri=config.REDIRECT_URI)
 except:
     os.remove(f".cache-{username}")
     token = util.prompt_for_user_token(username, scope, client_id=config.CLIENT_ID, 
                                        client_secret=config.CLIENT_SECRET,
                                        redirect_uri=config.REDIRECT_URI)
 
-spotifyObject = spotipy.Spotify(auth=token)
+spotify_object = spotipy.Spotify(auth=token)
+
 
 class CurrentState:
     def __init__(self, id, rgb):
         self.id = id
         self.color = rgb
 
-    def setCurrentState(self, id, rgb):
+    def set_current_state(self, id, rgb):
         self.id = id
         self.color = rgb
+
 
 class Lamps:
     def __init__(self, color):
         self.bulb1 = Bulb(config.bulb1Ip)
         self.bulb2 = Bulb(config.bulb2Ip)
-
         self.bulb1.set_rgb(color[0], color[1], color[2])
         self.bulb2.set_rgb(color[0], color[1], color[2])
 
-    def setLampColor(self, color):
+    def set_lamp_color(self, color):
         self.bulb1.set_rgb(color[0], color[1], color[2])
         self.bulb2.set_rgb(color[0], color[1], color[2])
 
-def mainLoop(currentStateObj, lamps):
+
+def main_loop(current_state_obj, lamps):
     while True:
-        song = fetchNowPlayingInfo()
-        checkCurrentSong(currentStateObj, lamps, song)
+        song = fetch_now_playing_info()
+        check_current_song(current_state_obj, lamps, song)
         time.sleep(1)
 
-def checkCurrentSong(current, lamps, song):
+
+def check_current_song(current, lamps, song):
     if current.id != song[0]:
-        current.setCurrentState(song[0], song[1])
+        current.set_current_state(song[0], song[1])
         print(song)
-        lamps.setLampColor(song[1])
+        lamps.set_lamp_color(song[1])
     else:
         pass
 
-def fetchNowPlayingInfo():
-    currentPlaying = spotifyObject.current_user_playing_track()
+
+def fetch_now_playing_info():
+    current_playing = spotify_object.current_user_playing_track()
     try:
-        nowPlayingImageURL = currentPlaying.get('item').get('album').get('images')[0].get('url')
-        nowPlayingColor = imageToRGB(nowPlayingImageURL)
-        nowPlayingId = currentPlaying.get('item').get('id')
-        return [nowPlayingId, nowPlayingColor]
+        now_playing_image_url = current_playing.get('item').get('album').get('images')[0].get('url')
+        now_playing_color = image_to_rgb(now_playing_image_url)
+        now_playing_id = current_playing.get('item').get('id')
+        return [now_playing_id, now_playing_color]
     except:
         time.sleep(3)
-        return fetchNowPlayingInfo()
+        return fetch_now_playing_info()
 
 
-def imageToRGB(sourceImage):
-    urlretrieve(sourceImage, 'image.jpg')
+def image_to_rgb(source_image):
+    urlretrieve(source_image, 'image.jpg')
 
     color_thief = ColorThief('image.jpg')
 
@@ -84,8 +88,9 @@ def imageToRGB(sourceImage):
     dominant_color = color_thief.get_color(quality=1)
     return dominant_color
 
-currentState = CurrentState(fetchNowPlayingInfo()[0],fetchNowPlayingInfo()[1])
+
+currentState = CurrentState(fetch_now_playing_info()[0], fetch_now_playing_info()[1])
 
 lamps = Lamps(currentState.color)
 
-mainLoop(currentState, lamps)
+main_loop(currentState, lamps)
